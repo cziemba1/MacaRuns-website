b5d31055ed1e8d489e97e59bd901de57e69fb628
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-02-28 00:05:35 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const menu = document.querySelector("#menu");
 const menuClose = document.querySelector(".menu-close");
-const productClose = document.querySelector(".product-img");
-const productHover = document.querySelector(".product-hover");
 
 
 menuClose.addEventListener("click", () => {
@@ -9,10 +7,7 @@ menuClose.addEventListener("click", () => {
     menu.classList.toggle("open");
 });
 
-productClose.addEventListener("click", () => {
-    productClose.classList.toggle("none");
-    productHover.classList.toggle("open");
-});
+
 
 
 
